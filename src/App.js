a5d8ab4ca3922ec9d89a49fad7c186ca4ec4db54
asr <ajsoriar@gmail.com>
2018-01-20 20:55:14 +0100

    Add a Row component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AppHeader from './components/AppHeader/AppHeader.js';
 import SideMenu from './components/SideMenu/SideMenu.js';
 import MainContainer from './components/MainContainer/MainContainer.js';
 import SquareItem from './components/items/SquareItem/SquareItem.js';
+import RowItem from './components/items/RowItem/RowItem.js';
 
 class App extends Component {
   render() {
@@ -19,6 +20,14 @@ class App extends Component {
         <SideMenu/>
 
         <SquareItem itemText="We try" size="300" />
+
+        <RowItem>
+            <SquareItem itemText="We try 1" size="100" />
+            <SquareItem itemText="We try 2" size="100" />
+            <SquareItem itemText="We try 3" size="100" />
+            <SquareItem itemText="We try 4" size="100" />
+            <SquareItem itemText="We try 5" size="100" />
+        </RowItem>
 
         <MainContainer>
           hello!
---
+++ src/components/items/RowItem/RowItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import './RowItem.css'
+ 
+class RowItem extends Component {
+
+    /*
+    constructor(props) {
+        super(props);
+        this.state = { counter: 1 };
+    }
+    */
+
+    render() {
+        return (
+            <div className="Row-item">
+            </div>
+        );
+    }
+}
+
+export default RowItem;
