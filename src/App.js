9ffbf53a7365cea82398cc76935c0a6563b5581e
asr <ajsoriar@gmail.com>
2018-01-21 18:28:33 +0100

    Top menu

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SideMenu from './components/SideMenu/SideMenu.js';
 import MainContainer from './components/MainContainer/MainContainer.js';
 import SquareItem from './components/items/SquareItem/SquareItem.js';
 import RowItem from './components/items/RowItem/RowItem.js';
+import HorizontalMenu from './components/HorizontalMenu/HorizontalMenu.js';
 
 class App extends Component {
   render() {
@@ -20,17 +21,19 @@ class App extends Component {
         {/* <SquareItem itemText="We try" size="300" /> */}
 
         <MainContainer content={
-            <RowItem content={
-                <span>
-                    <SquareItem itemText="We try 1" size="100" />
-                    <SquareItem itemText="We try 2" size="100" />
-                    <SquareItem itemText="We try 3" size="100" />
-                    <SquareItem itemText="We try 4" size="100" />
-                    <SquareItem itemText="We try 5" size="100" />
-                </span>
-            } />
+            <span>
+              <HorizontalMenu/>
+              <RowItem content={
+                  <span>
+                      <SquareItem itemText="We try 1" size="100" />
+                      <SquareItem itemText="We try 2" size="100" />
+                      <SquareItem itemText="We try 3" size="100" />
+                      <SquareItem itemText="We try 4" size="100" />
+                      <SquareItem itemText="We try 5" size="100" />
+                  </span>
+              } />
+            </span>
         } />
-
       </div>
     );
   }
